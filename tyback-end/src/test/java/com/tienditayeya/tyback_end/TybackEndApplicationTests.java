0868c093b248a30103695b33995cc38833e2e795
AriVeraI <verariadn@gmail.com>
2026-08-20 17:23:54 -0600

    Carpetas

---
+++ tyback-end/src/test/java/com/tienditayeya/tyback_end/TybackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienditayeya.tyback_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TybackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
